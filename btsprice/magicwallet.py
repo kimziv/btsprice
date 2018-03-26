573df05f548d047dafd475e8bde0d44beecc4405
kimziv <kimziv@qq.com>
2018-03-26 14:43:21 +0800

    change magic calculate method

--- btsprice/magicwallet.py
+++ btsprice/magicwallet.py
@@ -13,42 +13,6 @@ class Magicwallet():
             'apikey':config}
         self.session = aiohttp.ClientSession(headers=header)
 
-    
-    # @asyncio.coroutine
-    # def get_changerate(self):
-    #     try:
-    #         url = "https://redemption.icowallet.net/api_v2/RechargeAndWithdrawTables/GetListForRechargeAndWithdrawtable"
-    #         response = yield from asyncio.wait_for(self.session.post(url), 120)
-    #         response = yield from response.read()
-    #         result = json.loads(response.decode("utf-8-sig"))
-    #         wantpricerate=0
-    #         for pricelist in result:
-    #             if pricelist['datatype']=='1h':
-    #                 rbitcny=float(pricelist['depositBitCNY'])
-    #                 wbitcny=float(pricelist['withdrawBitCNY'])
-    #                 rfiatcny=float(pricelist['depositFiatCNY'])
-    #                 wfiatcny=float(pricelist['withdrawFiatCNY'])
-    #                 datelength=pricelist['datelength']
-    #                 if (rfiatcny+wfiatcny)==0:
-    #                     for pricelist24 in result:
-    #                         if pricelist24['datatype']=='24h':
-    #                             rbitcny=float(pricelist24['depositBitCNY'])
-    #                             wbitcny=float(pricelist24['withdrawBitCNY'])
-    #                             rfiatcny=float(pricelist24['depositFiatCNY'])
-    #                             wfiatcny=float(pricelist24['withdrawFiatCNY'])
-    #                             if (rfiatcny+wfiatcny)==0:
-    #                                 wantpricerate = 1
-    #                             else:
-    #                                 wantpricerate =round(float((rfiatcny+wfiatcny)/(rbitcny+wbitcny)),5)
-    #                 else:
-    #                     wantpricerate =float((rfiatcny+wfiatcny)/(rbitcny+wbitcny))
-    #         print(wantpricerate)
-    #         return wantpricerate
-    #     except Exception as e:
-    #         print("Error fetching book from binance!")
-    #         print(e)
-    #         return 1
-
     @asyncio.coroutine
     def get_changerate(self):
         try:
